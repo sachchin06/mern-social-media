7c1cc0c9242e4ed6bda52cfd2c0197ce80e64675
sachchin06 <sachchin06@gmail.com>
2022-11-19 16:07:53 +0530

    index.js updated for client/build

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan")
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
+const path = require('path');
 
 dotenv.config()
 
@@ -16,13 +17,26 @@ mongoose.connect(process.env.MONGO_URL, ()=> {
 
  //middleware
 app.use(express.json())
+
+// ADD THIS FOR DEPLOYMENT
+app.use(express.static('client/build'));
+
+
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"))
 
 app.get("/",(req,res)=>{
-    res.send("welcome")
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
+
+
+// If no API routes are hit, send the React app
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
 
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
